16dfe67dfe09af7c163de429d9a2ccb3d29dc572
Dan <ds.dan.shapiro@gmail.com>
2020-03-31 19:35:51 -0400

    Change post pagination to 2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		@posts = Post.all.order("created_at DESC").paginate(page: params[:page], per_page: 5)
+		@posts = Post.all.order("created_at DESC").paginate(page: params[:page], per_page: 2)
 		@tribes = Tribe.joins(:users).distinct.order("ordering")
 		@vote = Vote.joins(:tribes).where("tribes.id = ?", current_user.tribe_id).where(active: true).order("votes.updated_at DESC").first
 		@poll = Poll.new
